d6022de2d16528ad4100f3d0cd0116561f716c0a
Alessandro Maciel <alessandromaciel237@gmail.com>
2022-09-11 21:23:45 -0300

    Criando comando para integração

---
+++ app/Console/Commands/GitHub/ListRepositoryWorkflows.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands\GitHub;
+
+use Illuminate\Console\Command;
+
+class ListRepositoryWorkflows extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        return 0;
+    }
+}

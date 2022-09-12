ddb0b0bc21a3cb2eb062efebacbf734286d0a28d
Alessandro Maciel <alessandromaciel237@gmail.com>
2022-09-11 21:00:12 -0300

    Criando conector de integração com Github

---
+++ app/Http/Integrations/GitHub/GitHubConnector.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Integrations\GitHub;
+
+use Sammyjo20\Saloon\Http\SaloonConnector;
+use Sammyjo20\Saloon\Traits\Plugins\AcceptsJson;
+
+class GitHubConnector extends SaloonConnector
+{
+    use AcceptsJson;
+
+    /**
+     * The Base URL of the API.
+     *
+     * @return string
+     */
+    public function defineBaseUrl(): string
+    {
+        return '';
+    }
+
+    /**
+     * The headers that will be applied to every request.
+     *
+     * @return string[]
+     */
+    public function defaultHeaders(): array
+    {
+        return [];
+    }
+
+    /**
+     * The config options that will be applied to every request.
+     *
+     * @return string[]
+     */
+    public function defaultConfig(): array
+    {
+        return [];
+    }
+}

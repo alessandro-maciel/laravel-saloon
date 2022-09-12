5ef5d3b03264267fc6295228d0aec22108aa0736
Alessandro Maciel <alessandromaciel237@gmail.com>
2022-09-11 21:20:23 -0300

    Refatorando a classe de solicitação

--- app/Http/Integrations/GitHub/Requests/ListRepositoryWorkflowsRequest.php
+++ app/Http/Integrations/GitHub/Requests/ListRepositoryWorkflowsRequest.php
@@ -2,32 +2,24 @@
 
 namespace App\Http\Integrations\GitHub\Requests;
 
+use App\Http\Integrations\GitHub\GitHubConnector;
 use Sammyjo20\Saloon\Constants\Saloon;
 use Sammyjo20\Saloon\Http\SaloonRequest;
 
 class ListRepositoryWorkflowsRequest extends SaloonRequest
 {
-    /**
-     * The connector class.
-     *
-     * @var string|null
-     */
-    protected ?string $connector = null;
+    protected ?string $connector = GitHubConnector::class;
 
-    /**
-     * The HTTP verb the request will use.
-     *
-     * @var string|null
-     */
     protected ?string $method = Saloon::GET;
 
-    /**
-     * The endpoint of the request.
-     *
-     * @return string
-     */
+    public function __construct(
+        private string $owner,
+        private string $repo,
+    ) {
+    }
+
     public function defineEndpoint(): string
     {
-        return '/api/v1/user';
+        return "/repos/{$this->owner}/{$this->repo}/actions/workflows";
     }
 }

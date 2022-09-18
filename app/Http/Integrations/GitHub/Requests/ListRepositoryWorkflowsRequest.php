dfc837744809bfac012d5dd860d501856e9e80fb
Alessandro Maciel <alessandromaciel237@gmail.com>
2022-09-18 18:48:58 -0300

    Herdando a trait CastsToDto

--- app/Http/Integrations/GitHub/Requests/ListRepositoryWorkflowsRequest.php
+++ app/Http/Integrations/GitHub/Requests/ListRepositoryWorkflowsRequest.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Integrations\GitHub\Requests;
 
-use App\Http\Integrations\GitHub\GitHubConnector;
+use App\Http\Integrations\GitHub\DataObjects\Workflow;
 use Sammyjo20\Saloon\Constants\Saloon;
 use Sammyjo20\Saloon\Http\SaloonRequest;
+use Sammyjo20\Saloon\Http\SaloonResponse;
+use Sammyjo20\Saloon\Traits\Plugins\CastsToDto;
+use App\Http\Integrations\GitHub\GitHubConnector;
+use Illuminate\Support\Collection;
 
 class ListRepositoryWorkflowsRequest extends SaloonRequest
 {
+    use CastsToDto;
+
     protected ?string $connector = GitHubConnector::class;
 
     protected ?string $method = Saloon::GET;
@@ -21,5 +27,12 @@ class ListRepositoryWorkflowsRequest extends SaloonRequest
     public function defineEndpoint(): string
     {
         return "/repos/{$this->owner}/{$this->repo}/actions/workflows";
+    }
+
+    protected function castToDto(SaloonResponse $response): Collection
+    {
+        return collect($response->json('workflows'))->map(
+            fn ($workflow) => Workflow::fromSaloon($workflow)
+        );
     }
 }
